755470ad90d990bafe205ae5765b3ef0b0289cdb
Cesar Zulueta <cesarzulueta12@gmail.com>
2021-09-02 09:04:40 -0700

    built backend server with models and routes and all necessary basic components to start off with

---
+++ models/todo.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const todoSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    descripiton: {
+        type: String,
+        required: true
+    }
+})
+module.exports = mongoose.model("Todo", todoSchema)
